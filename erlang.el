57d5382b96784c4d9d4dcdb2620a4756ccaa5a27
Alexander Q <qalexx@gmail.com>
2016-09-17 02:57:43 +0300

    some erlang stuff

--- erlang.el
+++ erlang.el
@@ -1,6 +1,12 @@
+(use-package auto-highlight-mode
+  :config
+  (eval-after-load "auto-highlight-symbol-mode"
+    (progn (protect-my-bindings auto-highlight-symbol-mode-map))))
+
 (use-package edts
   :ensure t
   :config
+  (require 'edts-start)
   (defun edts-man--fetch-vsns ()
     (with-current-buffer (url-retrieve-synchronously edts-man-download-url)
       (goto-char (point-min))
@@ -11,13 +17,7 @@
           (re-search-forward re nil 'move-point)
           (push (cons (match-string 2) (match-string 1)) vsn-urls))
         (kill-buffer)
-        vsn-urls)))
-
-  (add-hook 'erlang-mode-hook
-            (lambda ()
-              (company-mode 0)
-              (auto-complete-mode 1)
-              (flycheck-mode 1))))
+        vsn-urls))))
 
 (use-package erlang
   :ensure t
@@ -25,15 +25,16 @@
   (eval-after-load "erlang-mode"
     (progn
       (define-key erlang-mode-map (kbd ">") nil)
-      (define-key erlang-mode-map (kbd "<RET>") 'newline-and-indent)
-      (require 'edts-start)))
+      (define-key erlang-mode-map (kbd "<RET>") 'newline-and-indent)))
   ;; (setq erlang-root-dir "/usr/local/Cellar/erlang/19.0.2/lib/erlang")
   ;; (add-to-list 'exec-path "/usr/local/Cellar/erlang/19.0.2/lib/erlang/bin")
 
   ;; prevent annoying hang-on-compile
   (defvar inferior-erlang-prompt-timeout t)
   ;; default node name to emacs@localhost
-  (setq inferior-erlang-machine-options '("-name" "emacs@localhost")))
+  (setq inferior-erlang-machine-options '("-name" "emacs@localhost"))
+  (setq erl-nodename-cache (make-symbol "emacs@localhost"))
+  )
 
 (use-package flycheck-dialyzer
   :ensure t
