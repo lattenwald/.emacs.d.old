9edefb10568928e13c34d361c8f5a499758a5e4f
Alexander Q <qalexx@gmail.com>
2017-09-12 16:21:25 +0300

    no need to byte-compile stuff; rust-mode-hook: added flycheck-mode

--- staging.el
+++ staging.el
@@ -82,7 +82,7 @@
   :config
   (add-hook 'rust-mode-hook
             (lambda ()
-              (local-set-key (kbd "C-c <tab>") #'rust-format-buffer))))
+              (local-set-key (kbd "C-c <tab>") 'rust-format-buffer))))
 
 (use-package cargo
   :ensure t)
@@ -90,14 +90,15 @@
 (use-package racer
   :ensure t
   :config
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-  (add-hook 'racer-mode-hook #'company-mode))
+  (add-hook 'rust-mode-hook 'racer-mode)
+  (add-hook 'racer-mode-hook 'eldoc-mode)
+  (add-hook 'racer-mode-hook 'company-mode))
 
 (use-package flycheck-rust
   :ensure t
   :config
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+  (add-hook 'flycheck-mode-hook 'flycheck-rust-setup)
+  (add-hook 'rust-mode-hook 'flycheck-mode))
 
 (defun insert-BOM()
   "Insert the BOM at the start of a file for UTF"
