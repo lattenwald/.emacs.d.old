e79466f5cddae6e6acb87172e4ffb8a9d85eda12
Alexander Q <qalexx@gmail.com>
2017-11-09 17:18:43 +0300

    settings stuff

--- custom.el
+++ custom.el
@@ -8,11 +8,11 @@
  '(ag-highlight-search t)
  '(ag-ignore-list
    (quote
-    ("bower_components" "output" "abbrev_defs" "bookmarks" "edts" "smex-items" "ido.last")))
+	("bower_components" "output" "abbrev_defs" "bookmarks" "edts" "smex-items" "ido.last")))
  '(ag-reuse-buffers t)
  '(backup-by-copying t)
  '(backup-directory-alist (quote (("." . "~/.emacs-backups"))))
- '(bmkp-last-as-first-bookmark-file "/Users/aleksandrkusev/.emacs.d/bookmarks")
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(bookmark-version-control (quote nospecial))
  '(browse-url-browser-function (quote browse-url-chrome))
  '(company-dabbrev-downcase nil)
@@ -31,11 +31,12 @@
  '(cua-mode t nil (cua-base))
  '(custom-safe-themes
    (quote
-    ("5eb4b22e97ddb2db9ecce7d983fa45eb8367447f151c7e1b033af27820f43760" "6254372d3ffe543979f21c4a4179cd819b808e5dd0f1787e2a2a647f5759c1d1" "57d7e8b7b7e0a22dc07357f0c30d18b33ffcbb7bcd9013ab2c9f70748cfa4838" "595099e6f4a036d71de7e1512656e9375dd72cf60ff69a5f6d14f0171f1de9c1" "9a3c51c59edfefd53e5de64c9da248c24b628d4e78cc808611abd15b3e58858f" "b4fd44f653c69fb95d3f34f071b223ae705bb691fb9abaf2ffca3351e92aa374" "f831c1716ebc909abe3c851569a402782b01074e665a4c140e3e52214f7504a0" "2ae4b0c50dd49a5f74edeae3e49965bf8853954b63c5712a7967ea0a008ecd5b" "5f4dfda04fbf7fd55228266c8aab73953d3087cea7fd06dd7f8ff1e4a497c739" "b0ab5c9172ea02fba36b974bbd93bc26e9d26f379c9a29b84903c666a5fde837" "e56ee322c8907feab796a1fb808ceadaab5caba5494a50ee83a13091d5b1a10c" default)))
+	("5eb4b22e97ddb2db9ecce7d983fa45eb8367447f151c7e1b033af27820f43760" "6254372d3ffe543979f21c4a4179cd819b808e5dd0f1787e2a2a647f5759c1d1" "57d7e8b7b7e0a22dc07357f0c30d18b33ffcbb7bcd9013ab2c9f70748cfa4838" "595099e6f4a036d71de7e1512656e9375dd72cf60ff69a5f6d14f0171f1de9c1" "9a3c51c59edfefd53e5de64c9da248c24b628d4e78cc808611abd15b3e58858f" "b4fd44f653c69fb95d3f34f071b223ae705bb691fb9abaf2ffca3351e92aa374" "f831c1716ebc909abe3c851569a402782b01074e665a4c140e3e52214f7504a0" "2ae4b0c50dd49a5f74edeae3e49965bf8853954b63c5712a7967ea0a008ecd5b" "5f4dfda04fbf7fd55228266c8aab73953d3087cea7fd06dd7f8ff1e4a497c739" "b0ab5c9172ea02fba36b974bbd93bc26e9d26f379c9a29b84903c666a5fde837" "e56ee322c8907feab796a1fb808ceadaab5caba5494a50ee83a13091d5b1a10c" default)))
  '(delete-old-versions t)
  '(edts-api-async-node-init t)
  '(edts-man-root "/Users/aleksandrkusev/.emacs.d/edts/doc/19.0")
  '(elm-compile-arguments (quote ("--yes" "--warn" "--output=elm.js")))
+ '(erlang-indent-level 2)
  '(ess-ask-for-ess-directory nil)
  '(exec-path-from-shell-arguments (quote ("-l")))
  '(fixmee--listview-local-only t t)
@@ -72,7 +73,6 @@
  '(ido-buffer-disable-smart-matches nil)
  '(ido-enable-flex-matching t)
  '(ido-use-faces nil)
- '(indent-tabs-mode nil)
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(js-indent-level 2)
  '(js2-basic-offset 2)
@@ -97,7 +97,7 @@
  '(osx-browse-prefer-browser "com.google.Chrome")
  '(package-selected-packages
    (quote
-    (projectile-ripgrep ripgrep-projectile ripgrep dumb-jump haskell-mode flycheck-rust racer cargo rust-mode osx-browse ag sqlup-mode sql-indent linum-off neotree racket-mode elm-yasnippets inf-ruby erlang yasnippet flycheck-dialyxir yagist web-mode magit flycheck-mix emojify atom-one-dark-theme org-jira hledger-mode flycheck-ledger pcre2el bookmark+ buttercup evil-mode color-theme-modern yaml-mode vimrc-mode undo-tree sunrise-x-checkpoints spaceline smart-tabs-mode projectile perlbrew nginx-mode markdown-mode lua-mode ledger-mode js2-mode idomenu go-mode gitignore-mode flycheck-elm flycheck-dialyzer flx-ido fixmee exec-path-from-shell elm-mode dockerfile-mode diminish apache-mode alchemist)))
+	(macro-math cmake-mode kerl sunrise-x-checkpoints ht sunrise-commander company projectile-ripgrep ripgrep-projectile ripgrep dumb-jump haskell-mode flycheck-rust racer cargo rust-mode osx-browse ag sqlup-mode sql-indent linum-off neotree racket-mode elm-yasnippets inf-ruby erlang yasnippet flycheck-dialyxir yagist web-mode magit flycheck-mix emojify atom-one-dark-theme org-jira hledger-mode flycheck-ledger pcre2el bookmark+ buttercup evil-mode color-theme-modern yaml-mode vimrc-mode undo-tree spaceline smart-tabs-mode projectile perlbrew nginx-mode markdown-mode lua-mode ledger-mode js2-mode idomenu go-mode gitignore-mode flycheck-elm flycheck-dialyzer flx-ido fixmee exec-path-from-shell elm-mode dockerfile-mode diminish apache-mode alchemist)))
  '(pcre-mode nil)
  '(powerline-height 20)
  '(projectile-global-mode t)
@@ -136,4 +136,4 @@
  '(fixmee-notice-face ((t (:background "yellow1" :foreground "black"))))
  '(flycheck-error ((t (:underline "Red1"))))
  '(flycheck-warning ((t (:underline "DarkOrange"))))
- '(whitespace-trailing ((t (:background "gray37" :foreground "yellow" :weight bold)))))
+ '(whitespace-trailing ((t nil))))
